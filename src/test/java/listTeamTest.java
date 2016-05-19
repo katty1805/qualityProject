20d47bde81dcc6414857146619fd3cb447e6e736
Katty Flores <kattyf_@hotmail.com>
2016-05-19 17:24:56 +0100

    Add Id TeamName

--- src/test/java/listTeamTest.java
+++ src/test/java/listTeamTest.java
@@ -1,5 +1,62 @@
 /**
  * Created by katty on 19/05/2016.
  */
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class listTeamTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new HtmlUnitDriver();
+        baseUrl = "http://softwareteam.azurewebsites.net/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testPrueba11() throws Exception {
+        driver.get(baseUrl + "/");
+        // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | frame | ]]
+        try {
+            assertEquals("Counter team", driver.findElement(By.cssSelector("font")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals("Christian Andrés Bustamante Crespo", driver.findElement(By.xpath("//p[2]/b/font")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals("Daniela Katherine Flores Taipe", driver.findElement(By.xpath("//p[3]/b/font")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+        try {
+            assertEquals("Daniel Augusto Vera Yanes", driver.findElement(By.xpath("//p[4]/b/font")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
 }
