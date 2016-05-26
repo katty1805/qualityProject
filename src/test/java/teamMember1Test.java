1905bf94061905b9cbb6ed858598a974db19111b
Christian Bustamante <c.a.bustamante@live.com>
2016-05-26 20:41:13 +0100

    add andres web page and andres test

---
+++ src/test/java/teamMember1Test.java
@@ -0,0 +1,81 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by danielvera on 5/26/16.
+ */
+public class teamMember1Test {
+
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    private String teamMemberName = "Christian Andr&eacutes Bustamante Crespo";
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new HtmlUnitDriver();
+        //baseUrl = "http://softwareteam.azurewebsites.net/";
+        //baseUrl="http: //localhost:8080/team";
+
+        baseUrl= useRemote();
+        System.out.println(baseUrl);
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    public String useRemote() {
+        String remote;
+        remote = System.getProperty("url");
+        if (remote == null)
+            return "http://localhost:8080/team";
+        else
+            return remote;
+    }
+
+    @Test
+    public void testMemberTitleName() throws Exception {
+        driver.get(baseUrl + "/");
+        changeDriverToFrameWithID("teamFrameID");
+        try {
+            WebElement memberLink = driver.findElement(By.partialLinkText(teamMemberName));
+            memberLink.click();
+
+            WebElement memberTitle = driver.findElement(By.id("memberTitle"));
+            assertEquals(teamMemberName, memberTitle.getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private void changeDriverToFrameWithID(String frameName)
+    {
+        try {
+            driver.switchTo().defaultContent();
+            driver.switchTo().frame(frameName);
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+}
+
