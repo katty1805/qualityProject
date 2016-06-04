36bac8532231fa278df8107040fb5b50f85c75a4
Christian Bustamante <c.a.bustamante@live.com>
2016-06-04 15:45:51 +0100

    Organize and clasificate TESTs by Semantic

--- src/test/java/christian_tests.java
+++ src/test/java/christian_tests.java
@@ -37,58 +37,148 @@ public class christian_tests {
     }
 
     @Test
-    public void testCompleteChristianProfile() throws Exception {
+    public void index_Text_test() throws  Exception{
         driver.get(baseUrl);
-        assertEquals("COUNTER QUALITY TEAM", driver.findElement(By.cssSelector("h1")).getText());
-        assertEquals("Software Quality Testers", driver.findElement(By.cssSelector("h2")).getText());
-        assertTrue(isElementPresent(By.cssSelector("figure > img")));
-        assertEquals("Christian Bustamante", driver.findElement(By.cssSelector("#christian_member1 > h2")).getText());
-        assertEquals("Informatic Engineering specialist in virtual reality and game development.", driver.findElement(By.cssSelector("p")).getText());
-        driver.findElement(By.linkText("Read More »")).click();
-        assertEquals("COUNTER QUALITY TEAM", driver.findElement(By.cssSelector("h1")).getText());
-        assertEquals("Software Quality Testers", driver.findElement(By.cssSelector("h2")).getText());
-        assertTrue(isElementPresent(By.cssSelector("img")));
-        assertEquals("ABOUT ME", driver.findElement(By.cssSelector("figcaption > h2")).getText());
 
-        WebElement basicInfo = driver.findElement(By.id("chochoInfoID"));
-        assertTrue(basicInfo.getText().contains("ABOUT ME"));
-        assertTrue(basicInfo.getText().contains("Christian Andr&eacutes Bustamante Crespo"));
-        assertTrue(basicInfo.getText().contains("Informatic Enginnering at ESPE"));
-        assertTrue(basicInfo.getText().contains("29 years old Current Master degree student at IPL"));
-        assertTrue(basicInfo.getText().contains("Quito - Ecuador"));
+        WebElement webElement = driver.findElement(By.id("hgroup"));
+        assertTrue(webElement.getAttribute("h1").contains("COUNTER QUALITY TEAM"));
+        assertTrue(webElement.getAttribute("h2").contains("Software Quality Testers"));
 
-        assertTrue(isElementPresent(By.cssSelector("#session2 > article.one_third > figure > img")));
-        assertTrue(isElementPresent(By.cssSelector("#Christian_homepage article.one_third figure img")));
-        assertEquals("SKILLS", driver.findElement(By.cssSelector("#Christian_homepage article.one_third.lastbox figcaption h2")).getText());
-        assertEquals("PROGRAMMING LANGUAGES: C, C++, C#, Java, Objective-C, Swift,javascript, Phyton, PHP, HTML", driver.findElement(By.cssSelector("p")).getText());
-        assertEquals("IDES: Visual Studio, Xcode, Netbeans, Eclipse, Android Studio, Xamarin, IntelliJ IDEA, Unity3D, Unreal Engine 4, Autodesk Maya, Autodesk Mudbox, Adobe Ilustrador, Adobe Photoshop", driver.findElement(By.xpath("//section[@id='session2']/article[2]/figcaption/p[2]")).getText());
+        webElement = driver.findElement(By.id("christian_member1"));
+        assertTrue(webElement.getAttribute("h2").contains("Christian Bustamante"));
+        assertTrue(webElement.getAttribute("p").contains("Informatic Engineering specialist in virtual reality and game development."));
 
-        WebElement developedProyects = driver.findElement(By.id("developedProyectsID"));
-        assertTrue(developedProyects.getText().contains("DEVELOPED PROYECTS"));
+        webElement = driver.findElement(By.id("katty_member2"));
+        assertTrue(webElement.getAttribute("h2").contains("Daniela Flores"));
+        assertTrue(webElement.getAttribute("p").contains("Electronic Enginnering specialist in system network designer."));
 
-        WebElement video1 = driver.findElement(By.id("video_1"));
-        assertTrue(video1.getAttribute("src").contains("https://www.youtube.com/embed/VbK4XRSwBtY"));
+        webElement = driver.findElement(By.id("dany_member3"));
+        assertTrue(webElement.getAttribute("h2").contains("Daniel Vera"));
+        assertTrue(webElement.getAttribute("p").contains("Informatic Enginnering specialist in native mobile development."));
 
-        WebElement video2 = driver.findElement(By.id("video_2"));
-        assertTrue(video2.getAttribute("src").contains("https://www.youtube.com/embed/5_QHQ45pQZI"));
+        webElement = driver.findElement(By.id("linkToAndres"));
+        assertTrue(webElement.getText().contains("andres.html"));
 
-        WebElement video3 = driver.findElement(By.id("video_3"));
-        assertTrue(video3.getAttribute("src").contains("https://www.youtube.com/embed/Vxpgi4guwz4"));
+        webElement = driver.findElement(By.id("linkToKatty"));
+        assertTrue(webElement.getText().contains("katty.html"));
 
+        webElement = driver.findElement(By.id("linkTeam3Info"));
+        assertTrue(webElement.getText().contains("dani.html"));
 
-        assertTrue(isElementPresent(By.id("facebook_link_id")));
-        WebElement facebookLink = driver.findElement(By.id("facebook_link_id"));
-        assertTrue(facebookLink.getAttribute("href").contains("https://www.facebook.com/tribaludic"));
-
-        assertEquals("SHOW MORE", driver.findElement(By.cssSelector("center > h2")).getText());
-        assertEquals("Simulation, Virtual Reality and video Games proyect demos", driver.findElement(By.cssSelector("center > p")).getText());
-
-        assertTrue(isElementPresent(By.id("youtube_id")));
-        WebElement youtubeElement = driver.findElement(By.id("youtube_id"));
-        assertTrue(youtubeElement.getAttribute("href").contains("https://www.youtube.com/watch?v=7eynCtj_74g&index=1&list=UUA5uTzoQIV49CTr4ZJzuULw"));
-
+        webElement = driver.findElement(By.id("copyright"));
+        assertTrue(webElement.getAttribute("p").contains("Copyright &copy; 2016 - All Rights Reserved - Counter Quality Team"));
 
     }
+
+    @Test
+    public void index_Images_test() throws  Exception{
+        driver.get(baseUrl);
+
+        WebElement webElement = driver.findElement(By.id("slider"));
+        assertTrue(webElement.getAttribute("src").contains("images/IndexPicture.jpg"));
+
+        webElement = driver.findElement(By.id("imagenAndres"));
+        assertTrue(webElement.getAttribute("src").contains("images/andres.jpg"));
+
+        webElement = driver.findElement(By.id("imagenKattuy"));
+        assertTrue(webElement.getAttribute("src").contains("images/katty2.jpg"));
+
+        webElement = driver.findElement(By.id("imagenDanny"));
+        assertTrue(webElement.getAttribute("src").contains("images/danny.jpg"));
+
+    }
+
+    @Test
+    public void index_Links_test() throws  Exception{
+        driver.get(baseUrl);
+
+        WebElement webElement = driver.findElement(By.id("linkToAndres"));
+        assertTrue(webElement.getAttribute("href").contains("andres.html"));
+
+        webElement = driver.findElement(By.id("linkToKatty"));
+        assertTrue(webElement.getAttribute("href").contains("katty.html"));
+
+        webElement = driver.findElement(By.id("linkTeam3Info"));
+        assertTrue(webElement.getAttribute("href").contains("dani.html"));
+    }
+
+    @Test
+    public void andres_Text_test() throws  Exception{
+        driver.get(baseUrl);
+
+        WebElement webElement = driver.findElement(By.id("hgroup"));
+        assertTrue(webElement.getAttribute("h1").contains("COUNTER QUALITY TEAM"));
+        assertTrue(webElement.getAttribute("h2").contains("Software Quality Testers"));
+
+        webElement = driver.findElement(By.id("aboutMeID"));
+        assertTrue(webElement.getText().contains("ABOUT ME"));
+        assertTrue(webElement.getText().contains("Christian Andr&eacutes Bustamante Crespo"));
+        assertTrue(webElement.getText().contains("Informatic Enginnering at ESPE"));
+        assertTrue(webElement.getText().contains("29 years old Current Master degree student at IPL"));
+        assertTrue(webElement.getText().contains("Quito - Ecuador"));
+
+        webElement = driver.findElement(By.id("skillsID"));
+        assertTrue(webElement.getText().contains("SKILLS"));
+        assertTrue(webElement.getText().contains("PROGRAMMING LANGUAGES: C, C++, C#, Java, Objective-C, Swift,javascript, Phyton, PHP, HTML"));
+        assertTrue(webElement.getText().contains("IDES: Visual Studio, Xcode, Netbeans, Eclipse, Android Studio, Xamarin, IntelliJ IDEA, Unity3D, Unreal Engine 4, Autodesk Maya, Autodesk Mudbox, Adobe Ilustrador, Adobe Photoshop"));
+
+        webElement = driver.findElement(By.id("developedProyectsID"));
+        assertTrue(webElement.getAttribute("h2").contains("DEVELOPED PROYECTS"));
+
+        webElement = driver.findElement(By.id("table1"));
+        assertTrue(webElement.getAttribute("h2").contains("SHOW MORE"));
+        assertTrue(webElement.getAttribute("p").contains("Simulation, Virtual Reality and video Games proyect demos"));
+
+        webElement = driver.findElement(By.id("copyright"));
+        assertTrue(webElement.getAttribute("p").contains("Copyright &copy; 2016 - All Rights Reserved - Counter Quality Team"));
+
+    }
+
+    @Test
+    public void andres_Images_test() throws  Exception{
+        driver.get(baseUrl);
+
+        WebElement webElement = driver.findElement(By.id("profilePicture"));
+        assertTrue(webElement.getAttribute("src").contains("images/andres.jpg"));
+
+        webElement = driver.findElement(By.id("skills_image_id"));
+        assertTrue(webElement.getAttribute("src").contains("images/languages.jpg"));
+
+        webElement = driver.findElement(By.id("facebook_logo_id"));
+        assertTrue(webElement.getAttribute("src").contains("images/facebook.png"));
+
+        webElement = driver.findElement(By.id("youtube_logo_id"));
+        assertTrue(webElement.getAttribute("src").contains("images/youtube_logo.png"));
+
+    }
+
+    @Test
+    public void andres_Videos_test() throws  Exception{
+        driver.get(baseUrl);
+
+        WebElement webElement = driver.findElement(By.id("video1"));
+        assertTrue(webElement.getAttribute("src").contains("https://www.youtube.com/embed/VbK4XRSwBtY"));
+
+        webElement = driver.findElement(By.id("video2"));
+        assertTrue(webElement.getAttribute("src").contains("https://www.youtube.com/embed/5_QHQ45pQZI"));
+
+        webElement = driver.findElement(By.id("video3"));
+        assertTrue(webElement.getAttribute("src").contains("https://www.youtube.com/embed/Vxpgi4guwz4"));
+
+    }
+
+    @Test
+    public void andres_Links_test() throws  Exception{
+        driver.get(baseUrl);
+
+        WebElement webElement = driver.findElement(By.id("facebook_link_id"));
+        assertTrue(webElement.getAttribute("href").contains("https://www.facebook.com/tribaludic"));
+
+        webElement = driver.findElement(By.id("youtube_id"));
+        assertTrue(webElement.getAttribute("href").contains("https://www.youtube.com/watch?v=7eynCtj_74g&index=1&list=UUA5uTzoQIV49CTr4ZJzuULw"));
+
+    }
+
 
     @After
     public void tearDown() throws Exception {
